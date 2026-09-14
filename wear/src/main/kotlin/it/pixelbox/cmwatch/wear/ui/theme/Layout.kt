c2118d24bed5f169b329758f45f39fbed1c05b94
frsorrentino <fr.sorrentino@gmail.com>
2026-09-15 01:08:44 +0200

    fix(wear): import the padding extensions morph needs

--- wear/src/main/kotlin/it/pixelbox/cmwatch/wear/ui/theme/Layout.kt
+++ wear/src/main/kotlin/it/pixelbox/cmwatch/wear/ui/theme/Layout.kt
@@ -1,5 +1,7 @@
 package it.pixelbox.cmwatch.wear.ui.theme
 
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
